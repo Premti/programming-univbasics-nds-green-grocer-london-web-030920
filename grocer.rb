7e478bd8764a940e4c012c281de3fb8386e7eb16
premti <premti@gmail.com>
2020-03-03 11:06:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
         :clearance => cart_item[:clearance]
       }
       cart << cart_item_with_coupon
-      
+      cart_item[]
    end
   end
   counter += 1
