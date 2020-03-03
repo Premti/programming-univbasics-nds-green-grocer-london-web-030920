24445090cee178a95cdade46fcc6c6afbe11caf3
premti <premti@gmail.com>
2020-03-03 11:05:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,7 +42,7 @@ def apply_coupons(cart, coupons)
       cart_item_with_coupon = {
         :item => couponed_item_name, 
         :price => coupons[counter][:cost] / coupons[counter][:num]
-        :count => 
+        :count => coupons
       }
    end
   end
